17afbdfee343fab9856aeea4f6600b5317116806
ericho <endonoho@gmail.com>
2024-05-22 16:05:35 -0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { latin } from './ui/fonts';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${latin.className} antialiased`}>{children}</body>
     </html>
   );
 }
